8819d1150b7739d4847d996e40619d70d5c10a5b
Bojo <bozhidar_zahariev@hotmail.com>
2019-06-01 14:35:58 +0100

    toggle button

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-let myLibrary = [{
-        title: "Vinetou",
-        author: "Karl May",
-        pages: 450,
-        status: "finished",
-    },
-    {
-        title: "It",
-        author: "Stephen King",
-        pages: 550,
-        status: "finished",
-    }
-];
-
-
+let myLibrary = [];
 
 
 // the constructor
@@ -39,6 +25,7 @@ console.log(listBooks(myLibrary));
 var submit = document.getElementById("submit");
 
 submit.addEventListener('click', () => {
+
     addBookToLibrary();
 
 });
@@ -54,13 +41,13 @@ function addBookToLibrary() {
     status = document.getElementById("myForm").elements[3].value;
     values.push(status);
 
-    book1 = new book(title, author, pages, status)
+    book1 = new book(title, author, pages, status);
     myLibrary.push(book1);
-    console.log(listBooks(myLibrary));
 
     var shelf = document.getElementById("books-container")
     var bookContainer = document.createElement("div")
     bookContainer.classList.add('bookContainer');
+    bookContainer.dataset.index = myLibrary.length;
     shelf.appendChild(bookContainer);
 
     for (let i = 0; i < 5; i++) {
@@ -70,5 +57,34 @@ function addBookToLibrary() {
         item.textContent = values[i];
     }
 
+
+    //Delete button
+    var btn = document.createElement("BUTTON");
+    btn.classList.add('btn-delete');
+    btn.textContent = bookContainer.dataset.index;
+    bookContainer.appendChild(btn);
+
+    btn.addEventListener('click', () => {
+        console.log(bookContainer.dataset.index);
+        //replace the obj with an empty str to keep the same index
+        myLibrary.splice(bookContainer.dataset.index - 1, 1, " ");
+        bookContainer.remove();
+        console.log(listBooks(myLibrary));
+    });
+
+
+    //toggle button 
+    var btn2 = document.createElement("BUTTON");
+    btn2.classList.add('btn-toggle');
+    btn2.textContent = "OFF"
+    bookContainer.appendChild(btn2);
+
+    btn2.addEventListener('click', () => {
+        if (btn2.textContent == "OFF") {
+            btn2.textContent = "ON";
+        } else if (btn2.textContent == "ON") {
+            btn2.textContent = "OFF"
+        }
+    });
 
 }
