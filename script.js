a3b4839dec9e3f0f63e82389934d16cedee92a4e
Bojo <bozhidar_zahariev@hotmail.com>
2019-06-06 14:03:06 +0100

    better better clearing the empty objects from the list

--- script.js
+++ script.js
@@ -20,14 +20,12 @@ function listBooks(books) {
 	return book_names;
 }
 
-
-
 var form = document.getElementById('myForm');
 
 //Submit button
 var submit = document.getElementById('submit');
 submit.addEventListener('click', () => {
-	if (form.elements[0].value !== "" && form.elements[1].value !== "" && form.elements[2].value !== "") {
+	if (form.elements[0].value !== '' && form.elements[1].value !== '' && form.elements[2].value !== '') {
 		addBookToLibrary();
 		newForm.style.display = 'none';
 	}
@@ -38,7 +36,6 @@ var cancel = document.getElementById('cancel');
 cancel.addEventListener('click', () => {
 	newForm.style.display = 'none';
 });
-
 
 //Add a new book trough the form
 function addBookToLibrary() {
@@ -61,19 +58,18 @@ var newForm = document.getElementById('newForm');
 var newEntryBtn = document.getElementById('new');
 newEntryBtn.addEventListener('click', () => {
 	newForm.style.display = 'block';
-	form.elements[0].value = "";
-	form.elements[1].value = "";
-	form.elements[2].value = "";
+	form.elements[0].value = '';
+	form.elements[1].value = '';
+	form.elements[2].value = '';
 });
-
 
 //On load
 function onStart() {
 	//Book1
 	values = [];
-	title = "Vinetou";
+	title = 'Vinetou';
 	values.push(title);
-	author = "Karl May";
+	author = 'Karl May';
 	values.push(author);
 	pages = 55;
 	values.push(pages);
@@ -83,9 +79,9 @@ function onStart() {
 
 	//Book2
 	values = [];
-	title = "Something else";
+	title = 'Something else';
 	values.push(title);
-	author = "Same one else";
+	author = 'Same one else';
 	values.push(author);
 	pages = 49;
 	values.push(pages);
@@ -95,9 +91,9 @@ function onStart() {
 
 	//Book3
 	values = [];
-	title = "Another thing";
+	title = 'Another thing';
 	values.push(title);
-	author = "Other author";
+	author = 'Other author';
 	values.push(author);
 	pages = 678;
 	values.push(pages);
@@ -105,9 +101,8 @@ function onStart() {
 	values.push(stat);
 	newBook();
 
-	listingBooks()
+	listingBooks();
 }
-
 
 function newBook() {
 	book1 = new book(title, author, pages, stat);
@@ -134,7 +129,7 @@ function newBook() {
 
 	btn.addEventListener('click', () => {
 		//replace the obj with an empty string to keep the same index
-		myLibrary.splice(bookContainer.dataset.index - 1, 1, "");
+		myLibrary.splice(bookContainer.dataset.index - 1, 1, '');
 		bookContainer.remove();
 		console.log(listBooks(myLibrary));
 		listingBooks();
@@ -158,25 +153,27 @@ function newBook() {
 			child[3].textContent = book1.stat;
 		}
 	});
-
 }
 
-//Displaying list 
+//Display list
 function listingBooks() {
-	const listContainer = document.getElementById("list");
+	const listContainer = document.getElementById('list');
+
+	//Clear the list every time to avoid stacking
 	while (listContainer.firstChild) {
 		listContainer.removeChild(listContainer.firstChild);
 	}
 
-	//New array copy of myLibrary, so to keep myLibrary indexes
+	//New array copy of myLibrary, so to keep myLibrary indexes intact
 	var arrayB = [];
 	var newArray = myLibrary.concat(arrayB);
 
 	//Clear the empty objects in the array
-	/*Array.prototype.remove = function () {
-		var what, a = arguments,
-			L = a.length,
-			ax;
+	Array.prototype.remove = function() {
+		var what = arguments;
+		var a = arguments;
+		var L = a.length;
+		var ax;
 		while (L && this.length) {
 			what = a[--L];
 			while ((ax = this.indexOf(what)) !== -1) {
@@ -186,24 +183,17 @@ function listingBooks() {
 		return this;
 	};
 
-	newArray.remove("");
-	*/
-
-	var itemInQuestion = "";
-	var index = newArray.indexOf(itemInQuestion);
-	if (index !== -1) {
-		newArray.splice(index, 1);
-	}
+	newArray.remove('');
 
 	//Display the list cleared of empty objects
 	for (let i = 0; i < newArray.length; i++) {
-		var listItem = document.createElement("li");
+		var listItem = document.createElement('li');
 		listItem.classList.add('listItem');
 		listContainer.appendChild(listItem);
 		listItem.textContent = newArray[i].title;
-
 	}
-}
 
+	console.log(newArray);
+}
 
 onStart();
